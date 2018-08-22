2e59d957523c5b95d5e40f475771bca81a6ff914
Hamilton Fonte II <hamiltonii@gmail.com>
2018-08-21 23:06:44 -0300

    tentativas frustradas de leitura

--- dumper.sh
+++ dumper.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-grep rw-p /proc/$1/maps | sed -n 's/^\([0-9a-f]*\)-\([0-9a-f]*\) .*$/\1 \2/p' | while read start stop; do gdb --batch --pid $1 -ex "dump memory $1-$start-$stop.dump 0x$start 0x$stop"; done
+#reading the memory of a process which is outside of the conteiner
+#grep rw-p /proc/$1/maps | sed -n 's/^\([0-9a-f]*\)-\([0-9a-f]*\) .*$/\1 \2/p' | while read start stop; do gdb --batch --pid $1 -ex "dump memory $1-$start-$stop.dump 0x$start 0x$stop"; done
+
+#attempting, so far without success, to read the memory of a process which is running inside a contêiner
+grep rw-p /proc/$1/maps | sed -n 's/^\([0-9a-f]*\)-\([0-9a-f]*\) .*$/\1 \2/p' | while read start stop; do docker -H tcp://127.0.0.1:2375 exec -i furious_newton /usr/bin/gdb --batch --pid $1 -ex "dump memory $1-$start-$stop.dump 0x$start 0x$stop"; done
 
