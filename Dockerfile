1831c10acdfe653d60886af6c3fa7ef260b1065f
Hamilton Fonte II <hamiltonii@gmail.com>
2019-04-25 22:12:32 -0300

    consegui ler conteudo cru da memoria do conteiner

--- Dockerfile
+++ Dockerfile
@@ -5,11 +5,13 @@ ENV DEBIAN_FRONTEND noninteractive
 RUN echo "#!/bin/sh\nexit 0" > /usr/sbin/policy-rc.d
 
 RUN apt-get -y update
+RUN apt-get -y -q install bc
 RUN apt-get -y -q install gdb
 Run apt-get -y -q install gcc
 
 RUN useradd -ms /bin/bash someuser
 
+ADD ./dumper.sh /home/someuser/dumper.sh
 ADD ./target.c /home/someuser/target.c
 ADD ./example.c /home/someuser/example.c
 
--- dumper.sh
+++ dumper.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+#lendo memoria do conteiner via nsenter
+grep rw-p /proc/$1/maps | sed -n 's/^\([0-9a-f]*\)-\([0-9a-f]*\) .*$/\1 \2/p' | while read start stop; do gdb --batch --pid $1 -ex "x/4096wx 0x$start"; done > $1.dump
+
 #lendo a memória do processo do contêiner
-grep rw-p /proc/$1/maps | sed -n 's/^\([0-9a-f]*\)-\([0-9a-f]*\) .*$/\1 \2/p' | while read start stop; do gdb --batch --pid $1 -ex "dump memory $1-$start-$stop.dump 0x$start 0x$stop"; done
+#grep rw-p /proc/$1/maps | sed -n 's/^\([0-9a-f]*\)-\([0-9a-f]*\) .*$/\1 \2/p' | while read start stop; do gdb --batch --pid $1 -ex "dump memory $1-$start-$stop.dump 0x$start 0x$stop"; done
 
 #attempting, so far without success, to read the memory of a process which is running inside a contêiner
 #grep rw-p /proc/$1/maps | sed -n 's/^\([0-9a-f]*\)-\([0-9a-f]*\) .*$/\1 \2/p' | while read start stop; do docker -H tcp://127.0.0.1:2375 exec -i furious_newton /usr/bin/gdb --batch --pid $1 -ex "dump memory $1-$start-$stop.dump 0x$start 0x$stop"; done
